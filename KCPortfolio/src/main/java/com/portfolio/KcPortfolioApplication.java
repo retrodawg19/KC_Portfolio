e4fb07417845397bb5ff7e6888843096abf77704
kunal_chopra_19 <118886961+retrodawg19@users.noreply.github.com>
2026-06-06 12:11:40 +0530

    SpringBoot Application- My Portfolio 1st commit

---
+++ KCPortfolio/src/main/java/com/portfolio/KcPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcPortfolioApplication.class, args);
+	}
+
+}
